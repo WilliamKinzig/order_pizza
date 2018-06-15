c864758fd800bdea9ea467cc3cb72bb6a094759a
William Kinzig <wippawill2@gmail.com>
2018-06-15 10:31:00 -0700

    add constructor, and variable to store value from 'pizza size' radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,18 @@ $(document).ready(function() {
   $("#button").click(function(){
     event.preventDefault();
 
-      var option1 = parseInt($("input:radio[name=quesGroup01]:checked").val());
-      var option2 = parseInt($("input:radio[name=quesGroup02]:checked").val());
-      var option3 = parseInt($("input:radio[name=quesGroup03]:checked").val());
+      //var option1 = parseInt($("input:radio[name=size]:checked").val());
+      //var option2 = parseInt($("input:radio[name=quesGroup02]:checked").val());
+      //var option3 = parseInt($("input:radio[name=quesGroup03]:checked").val());
 
       function Pizza(size, toppings) {
         this.size= size;
         this.toppings = toppings;
       }
 
+      var sizeIn = parseInt($("input:radio[name=size]:checked").val());
+
+      var pizza01 = new Pizza ("sizeIn",[]);
 
 
 
