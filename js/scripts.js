b9b4948556f726d47f443a443b8c1d3b3af41bd8
William Kinzig <wippawill2@gmail.com>
2018-06-15 12:31:46 -0700

    create array to store the total number of toppings on the pizza, this value will be used later in the calculation for total cost

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,66 @@ $(document).ready(function() {
 
       var sizeIn = parseInt($("input:radio[name=size]:checked").val());
       var pizzaSize = pizzaSize();
+
+      var amntPssblTppngs = 6;
       //var pizzaSize = sizeIn;
+
+
+      function getToppingTotalVal(){
+        var totalToppings = 0;
+        var toppings = [];
+        //alert(totalToppings);
+          var top1 = parseInt($("input:radio[name=toppings1]:checked").val());
+          var top2 = parseInt($("input:radio[name=toppings2]:checked").val());
+          var top3 = parseInt($("input:radio[name=toppings3]:checked").val());
+          var top4 = parseInt($("input:radio[name=toppings4]:checked").val());
+          var top5 = parseInt($("input:radio[name=toppings5]:checked").val());
+          var top6 = parseInt($("input:radio[name=toppings6]:checked").val());
+
+          if (top1){
+            toppings.push("1");
+          }
+          if (top2){
+            toppings.push("1");
+          }
+          if (top3){
+            toppings.push("1");
+          }
+          if (top4){
+            toppings.push("1");
+          }
+          if (top5){
+            toppings.push("1");
+          }
+          if (top6){
+            toppings.push("1");
+          }
+          alert(toppings);
+
+
+          for (i=0; i<toppings.length; i++){
+            totalToppings += 1;
+          }
+
+          alert(totalToppings);
+      }
+
+        //
+        // for (i = 0; i < amntPssblTppngs, i++)
+        //   var toppingsIn = parseInt($("input:radio[name=toppings]:checked").val());
+
+
       var pizza01 = new Pizza ("pizzaSize",[]);
-      var toppings = [];
+
+      getToppingTotalVal();
+
+      //alert("toppingsIn Value " + toppingsIn);
+
+
+
+      //orderSummary();
+      //var finalSummary = orderSummary();
+/****************** FUNCTIONS ******************/
 
       function Pizza(size, toppings) {
         this.size= size;
@@ -25,10 +82,17 @@ $(document).ready(function() {
           sizeOfPizza = "medium";
         }else if(sizeIn === 3){
           sizeOfPizza = "large";
-        }else alert("pick a size for you pizza");
+        }else //alert("pick a size for you pizza");
         //pizzaSize();
         return sizeOfPizza;
       }
+
+      // function toppingsCal(){
+      //   var toppingsArray = [];
+      //   var toppingsIn = parseInt($("input:radio[name=toppings]:checked").val());
+      //
+      // }
+
 
       // alert(sizeIn);
       // alert(pizzaSize);
@@ -40,8 +104,9 @@ $(document).ready(function() {
       // function orderSummary() {
       //
       //
-      //     toppings = ['london', 'ontario', 'san francisco', 'new york'];
+      //
       //     var finalToppings = [];
+
       //     for (var i = 0; i < toppings.length; i++) {
       //       finalToppings.push('<span>' + array[i] + '</span>');
       //     }
